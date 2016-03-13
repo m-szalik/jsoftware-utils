42047bf4e40b55479679e87ef2a51b27b304462a
m-szalik <szalik@jsoftware.org>
2016-03-13 17:02:37 +0100

    More tests

--- src/test/java/org/jsoftware/utils/TimeWatchTest.java
+++ src/test/java/org/jsoftware/utils/TimeWatchTest.java
@@ -2,25 +2,32 @@ package org.jsoftware.utils;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
 
 public class TimeWatchTest {
+    private TestClock clock;
+    private TimeWatch timeWatch;
+
+    @Before
+    public void setUp() throws Exception {
+        clock = new TestClock();
+        timeWatch = new TimeWatch(clock);
+    }
 
     @Test
     public void testMeasure() throws InterruptedException {
-        TimeWatch timeWatch = new TimeWatch();
-        Thread.sleep(1200);
+        clock.update(clock.instant().plus(1200, ChronoUnit.MILLIS));
         String ts = timeWatch.getDurationHuman();
         org.junit.Assert.assertTrue(ts.matches("\\d{4}ms\\."));
     }
 
     @Test
     public void testDuration() throws Exception {
-        TestClock clock = new TestClock();
-        TimeWatch timeWatch = new TimeWatch(clock);
         Instant instant = clock.instant().plus(12, ChronoUnit.MINUTES).plus(9, ChronoUnit.SECONDS);
         long diff = instant.toEpochMilli() - clock.millis();
         clock.update(instant);
@@ -32,11 +39,18 @@ public class TimeWatchTest {
 
     @Test
     public void testDurationHuman() throws Exception {
-        TestClock clock = new TestClock();
-        TimeWatch timeWatch = new TimeWatch(clock);
         Instant instant = clock.instant().plus(241, ChronoUnit.HOURS).plus(12, ChronoUnit.MINUTES).plus(9, ChronoUnit.SECONDS).plus(166, ChronoUnit.MILLIS);
         clock.update(instant);
         String duration = timeWatch.getDurationHuman();
         Assert.assertEquals("10d01:12:09.166", duration);
+    }
+
+    @Test
+    public void testReset() throws Exception {
+        clock.update(clock.instant().plus(9, ChronoUnit.SECONDS));
+        Assert.assertEquals(TimeUnit.SECONDS.toMillis(9), timeWatch.getDuration());
+        timeWatch.reset();
+        clock.update(clock.instant().plus(2, ChronoUnit.SECONDS));
+        Assert.assertEquals(TimeUnit.SECONDS.toMillis(2), timeWatch.getDuration());
     }
 }
